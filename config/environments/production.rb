ae1e79abd358e9c13d3597dbf88985cb50223667
mingliang7 <faithful.7up@gmail.com>
2014-07-09 21:23:53 +0700

    config fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-
+config.assets.precompile += %w( .svg .eot .woff .ttf )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
